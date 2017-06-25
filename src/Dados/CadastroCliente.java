c2442cde256065418f5902e89029fb1ef54a1782
alxtpimenta <alxtpimenta@icloud.com>
2017-06-24 23:56:07 -0300

    Commit Inicial

---
+++ src/Dados/CadastroCliente.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Dados;
+
+/**
+ *
+ * @author alex
+ */
+public class CadastroCliente extends Cadastro 
+{
+    protected String nome;
+    protected int cpf;
+    protected String email;
+    protected int telefone;
+}
---
+++ src/Dados/CadastroFuncionario.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Dados;
+
+/**
+ *
+ * @author alex
+ */
+public class CadastroFuncionario extends Cadastro 
+{
+    private String nome;
+    private String especialidade;
+    private String email;
+    private int telefone;
+}
